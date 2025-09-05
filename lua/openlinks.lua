13cd4b197fedca534f095fdecb22704d206b661e
rayan nadeem <rayan.nadeem@outlook.com>
2025-09-06 00:32:16 +0100

    trying it as a function now

--- lua/openlinks.lua
+++ lua/openlinks.lua
@@ -1,23 +1,34 @@
 local M = {}
 
 local function get_link_under_cursor()
-	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 	local line = tostring(vim.api.nvim_get_current_line())
 
 	-- simple URL regex (http, https, ftp)
 	local pattern = "https?://[%w-_%.%?%.:/%+=&]+"
-	
+
 	for w in string.gmatch(line, pattern) do
 		return w
 	end
 
--- 	for start_idx, end_idx in line:gmatch("()(" .. pattern .. ")()") do
--- 		-- check if cursor column is inside the match
--- 		if col + 1 >= start_idx and col < end_idx then
--- 			return line:sub(start_idx, end_idx - 1)
--- 		end
--- 	end
+	-- 	for start_idx, end_idx in line:gmatch("()(" .. pattern .. ")()") do
+	-- 		-- check if cursor column is inside the match
+	-- 		if col + 1 >= start_idx and col < end_idx then
+	-- 			return line:sub(start_idx, end_idx - 1)
+	-- 		end
+	-- 	end
 	return nil
+end
+
+M.openlink = function()
+	local link = get_link_under_cursor()
+	if not link then
+		print("No link under cursor")
+		return
+	end
+
+	-- escape URL properly
+	os.execute(string.format("xdg-open '%s' &", link))
+	print("Successfully opened link: " .. link)
 end
 
 M.setup = function()
@@ -33,8 +44,7 @@ M.setup = function()
 		-- escape URL properly
 		os.execute(string.format("xdg-open '%s' &", link))
 		print("Successfully opened link: " .. link)
-	end, {}) 
+	end, {})
 end
 
 return M
-
